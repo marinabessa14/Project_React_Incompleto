d5049159ce19722cda02f37c68f030e1470fafa6
marinabessa <marinabessarodrigues@hotmail.com>
2023-05-16 13:47:49 -0300

    Project-Incompleto

---
+++ src/Componentes/pages/NewProject.js
@@ -0,0 +1,19 @@
+import ProjectForm from '../project/ProjectForm'
+
+import styles from './NewProject.module.css'
+
+
+
+
+function NewProject () {
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm btnText="Criar Projeto"/>
+        </div>
+    )
+
+}
+
+export default NewProject 
---
+++ src/Componentes/project/ProjectForm.js
@@ -0,0 +1,43 @@
+import {useState} from 'react'
+import Input from '../form/Input'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
+import styles from './ProjectForm.module.css'
+
+
+function ProjectForm({btnText}) {
+    const {categories, setCategories} = useState([])
+
+    fetch("http://localhost:5000/categories", {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    } )
+    .then((resp) => resp.json())
+    .then((data) => {
+        setCategories(data)
+    })
+    .catch((err) => console.log(err))
+    return (
+        <form className={styles.form}>
+            <Input
+            type="text"
+            text="Nome do projeto"
+            name="name"
+            placeholder="Insira o nome do projeto"
+            />
+           <Input
+            type="number"
+            text="Orçamento do projeto"
+            name="budget"
+            placeholder="Insira o orçamento total"
+            />
+          <Select name="category_id" text="Selecione a categoria"/>
+           <SubmitButton text={btnText}/>
+        </form>
+
+    )
+}
+
+export default ProjectForm
